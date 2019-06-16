ca3b7f8a128c2d2f24f60f3bffba7960eb932281
Erfan Azad <erfan.basiri.azad@gmail.com>
2019-06-16 08:10:57 -0900

    added display

--- src/garden.py
+++ src/garden.py
@@ -1,6 +1,7 @@
 from time import sleep
 import RPi.GPIO as GPIO
 import Adafruit_DHT as DHT
+import Adafruit_CharLCD.Adafruit_CharLCD as LCD
 
 class GardN:
 	HIGH = GPIO.HIGH
@@ -8,6 +9,7 @@ class GardN:
 
 	def __init__(self, pin_mode=GPIO.BCM):
 		GPIO.setmode(pin_mode)
+		self.initialize_display()
 
 	def setup_output(self, pin):
 		GPIO.setup(pin, GPIO.OUT)
@@ -24,6 +26,16 @@ class GardN:
 	def read_DHT_sensor(self, pin, sensor_type=DHT.DHT11):
 		return DHT.read_retry(sensor_type, pin)
 
+	def initialize_display(self):
+		self.lcd = LCD(rs=26, en=19, d4=13, d5=6, d6=5, d7=11, cols=16, lines=2)
+		self.lcd.clear()
+		self.lcd.message('Starting...')
+		sleep(2)
+
+	def print_on_display(self, message):
+		self.lcd.clear()
+		self.lcd.message(message)
+
 	def cleanup(self):
 		GPIO.cleanup()
 
@@ -37,14 +49,15 @@ if __name__ == "__main__":
 		while True:
 			humidity, temperature = garden.read_DHT_sensor(17)
 			print(f'humidity: {humidity} | temperature: {temperature}')
+			garden.print_on_display(f'Temperature: {int(temperature)}\nHumidity:    {int(humidity)}%')
 
-			if humidity > 18:
-				print('setting 18 low')
+			if humidity>90 or temperature>30:
+				print('setting 18 low: Atomizer OFF')
 				garden.set_pin(18, garden.LOW)
 			else:
-				print('setting 18 high')
+				print('setting 18 high: Atomizer ON')
 				garden.set_pin(18, garden.HIGH)
-			sleep(1)
+			sleep(5)
 
 	except Exception as ex:
 		print(f'**** ERROR ****\n{ex}')
