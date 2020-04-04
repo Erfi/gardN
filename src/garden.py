1ddb9239ce76339f9ec2cc471c0b64e399ad8b91
Erfan Azad <erfan.basiri.azad@gmail.com>
2020-04-04 08:11:58 -0900

    using python logging

--- src/garden.py
+++ src/garden.py
@@ -1,6 +1,7 @@
+import random
+import logging
 from time import sleep
 from datetime import datetime
-import random
 import RPi.GPIO as GPIO
 from dht_sensor import DHT_sensor
 from display import Display
@@ -10,36 +11,41 @@ from project_paths import LOG_PATH
 
 
 class GardN:
-	HIGH = GPIO.HIGH
-	LOW = GPIO.LOW
+    HIGH = GPIO.HIGH
+    LOW = GPIO.LOW
 
-	def __init__(self, atomizer_pin, dht_pin, display_settings):
-		GPIO.setmode(GPIO.BCM)
-		self.atomizer = Atomizer(pin=atomizer_pin)
-		self.dht = DHT_sensor(pin=dht_pin)
-		self.display = Display(**display_settings)
+    def __init__(self, atomizer_pin, dht_pin, display_settings):
+        GPIO.setmode(GPIO.BCM)
+        self.atomizer = Atomizer(pin=atomizer_pin)
+        self.dht = DHT_sensor(pin=dht_pin)
+        self.display = Display(**display_settings)
 
-	def log(self, message):
-		with open(LOG_PATH / 'log.txt', 'a') as fp:
-			fp.write(f'{message}\n')
-
-	def cleanup(self):
-		GPIO.cleanup()
+    def cleanup(self):
+        GPIO.cleanup()
 
 
 if __name__ == "__main__":
-	try:
-		garden = GardN(atomizer_pin=23,
-						dht_pin=18,
-						display_settings={'rs':26, 'en':19, 'd4':13, 'd5':6, 'd6':5, 'd7':11, 'cols':16, 'lines':2})
-		while True:
-			humidity, temperature = garden.dht.read_sensor_data()
-			garden.log(f'{datetime.now()} | Temperature: {temperature} Humidity:    {humidity}%')
-		        garden.atomizer.pulse(sec_on=60, sec_off=300)
+    # setup the logger
+    logging.basicConfig(filename=str(LOG_PATH / 'log.txt'),
+                            filemode='a', 
+                            format='%(levelname)s - %(asctime)s - %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S', 
+                            level=logging.INFO)
+    
+    try:
+        logging.info('Initializing')
+        garden = GardN(atomizer_pin=23,
+                       dht_pin=18,
+                       display_settings={'rs':26, 'en':19, 'd4':13, 'd5':6, 'd6':5, 'd7':11, 'cols':16, 'lines':2})
+        
+        while True:
+            # humidity, temperature = garden.dht.read_sensor_data()
+            # garden.log(f'{datetime.now()} | Temperature: {temperature} Humidity:    {humidity}%')
+            garden.atomizer.pulse(sec_on=2, sec_off=2)
 
-	except Exception as ex:
-		garden.log(str(ex))
+    except Exception as ex:
+        logging.error('Exception occurred', exc_info=True)
 
-	finally:
-		garden.log('cleaning up')
-		garden.cleanup()
+    finally:
+        logging.info('cleaning up')
+        garden.cleanup()
